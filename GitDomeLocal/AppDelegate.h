5ca31d79473883af8d021677580a3023b9540cdf
zhushiqi <18710943589@163.com>
2019-04-26 13:38:56 +0800

    Initial Commit

---
+++ GitDomeLocal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDomeLocal
+//
+//  Created by apple on 2019/4/26.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDomeLocal/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GitDomeLocal
+//
+//  Created by apple on 2019/4/26.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
