d2ff48f0c9c12ce04802e4a643679fcbde5c0845
zhushiqi <3231946131@qq.com>
2019-08-14 14:19:19 +0800

    22

---
+++ GitDomeLocal/设计模式/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  GitDomeLocal
+//
+//  Created by apple on 2019/8/14.
+//  Copyright © 2019 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
